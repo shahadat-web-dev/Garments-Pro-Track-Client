f0c9c1f7b2ceda593b7fa5df2005fe372884add3
shahadat-web-dev <misssharamoni@gamil.com>
2025-12-05 23:41:39 +0600

    add footer navber section

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,8 +1,16 @@
 import { createBrowserRouter } from "react-router";
+import RootLayout from "../layout/RootLayout";
+import Home from "../pages/Home/home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home
+      }
+    ]
   },
 ]);
